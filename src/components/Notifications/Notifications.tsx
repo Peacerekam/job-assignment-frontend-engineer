e44524a1abaccba700ab12b3435971e52124746a
Kamil Masłowiec <kpmaslowiec@gmail.com>
2022-10-18 19:38:26 +0200

    fix style

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./style.scss"
 
 export const Notifications: React.FC = () => {
   const [notification, setNotification] = useState<string>();
